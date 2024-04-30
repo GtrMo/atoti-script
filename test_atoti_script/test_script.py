7c573e1d48ff924559be00b40712613dc9206fdf
Gautier Moin <gmo@activeviam.com>
2024-04-30 15:44:27 +0200

    Update atoti

---
+++ test_atoti_script/test_script.py
@@ -0,0 +1,17 @@
+import unittest
+
+import atoti as tt
+from atoti_script.init import start_application, CUBE_NAME
+
+
+class TestScript(unittest.TestCase):
+    def test_script(self):
+        with tt.Session() as session:
+            start_application(session)
+
+            cube = session.cubes[CUBE_NAME]
+            cube = session.cubes[CUBE_NAME]
+            self.assertEqual(
+                cube.query(cube.measures["Cumulative amount"])["Cumulative amount"][0],
+                961_463,
+            )
